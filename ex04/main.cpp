74deee886b8d4857be9871c90d45717e7ef1edcb
Armand Mosnier <armosnie@f4r2s8.clusters.42paris.fr>
2025-11-27 16:58:18 +0100

    ex04

---
+++ ex04/main.cpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: armosnie <armosnie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/27 13:46:58 by armosnie          #+#    #+#             */
+/*   Updated: 2025/11/27 16:38:32 by armosnie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+std::string replaceAll(std::string content, std::string s1, std::string s2){
+    
+    std::string result;
+    size_t pos = 0;
+    size_t found;
+
+    while ((found = content.find(s1, pos)) != std::string::npos){
+        result += content.substr(pos, found - pos);
+        result += s2;
+        pos = found + s1.length();
+    }
+    return result;
+}   
+
+
+int main(int argc, char **argv)
+{
+    if (argc != 4)
+        return 1;
+    std::string file = argv[1];
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
+    std::ifstream infile(file);
+    if (!infile.is_open()){
+        std::cout << "opening file failed" << std::endl;
+        return 1;
+    }
+    std::string line;
+    std::string content;
+    while (std::getline(infile, line))
+    {
+        content+= line;
+        if (!infile.eof())
+            content+= "\n"; 
+    }
+    infile.close();
+    std::string replaceAll(std::string content, std::string to_find, std::string text);
+}
