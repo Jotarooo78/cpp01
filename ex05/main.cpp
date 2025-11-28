8231e9eaf4fe4b414959e737f034095099378944
Armand Mosnier <armosnie@f4r2s8.clusters.42paris.fr>
2025-11-28 15:28:49 +0100

    ex05 finished

---
+++ ex05/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: armosnie <armosnie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/28 12:15:56 by armosnie          #+#    #+#             */
+/*   Updated: 2025/11/28 15:25:53 by armosnie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main()
+{
+    Harl tab;
+    tab.complain("DEBUG");
+    tab.complain("INFO");
+    tab.complain("WARNING");
+    tab.complain("ERROR");
+    return 0;
+}
